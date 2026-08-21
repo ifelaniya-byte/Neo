784e3bc60944c1391fc2e5317ef19f15971a58ff
ifelaniya-byte <ifelaniya@gmail.com>
2026-08-21 18:57:56 -0400

    SROS: export capability registry

--- 01_PROJECTS/SROS/src/sros/__init__.py
+++ 01_PROJECTS/SROS/src/sros/__init__.py
@@ -1,9 +1,13 @@
 """State Resolutions Operation Solver."""
 
+from .capabilities import Capability, CapabilityRegistry, default_capabilities
 from .models import OperationState, Resolution, ResolutionResult, ValidationReport
 from .solver import SROS, ResolutionPolicy
 
 __all__ = [
+    "Capability",
+    "CapabilityRegistry",
+    "default_capabilities",
     "OperationState",
     "Resolution",
     "ResolutionResult",
